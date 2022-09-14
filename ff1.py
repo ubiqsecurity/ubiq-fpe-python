8c2ec60c12c20942614a1e8a91a4dc888a81d7b9
John Tyner <jtyner@ubiqsecurity.com>
2022-09-13 22:10:46 -0400

    simplify some code; add constants

--- ff1.py
+++ ff1.py
@@ -8,7 +8,9 @@ class Context:
     def __init__(self, key, twk, mintwklen, maxtwklen, radix):
         self.ffx = ffx.Context(key, twk, 2**32, mintwklen, maxtwklen, radix)
 
-    def cipher(self, X, T, enc):
+    def cipher(self, X, T, ENC):
+        BLKSZ = ffx.AES_BLOCK_SIZE
+
         n = len(X)
         u = int(n / 2)
         v = n - u
@@ -16,8 +18,7 @@ class Context:
         b = int((math.ceil(math.log2(self.ffx.radix) * v) + 7) / 8)
         d = 4 * int((b + 3) / 4) + 4
 
-        p = 16
-        r = int((d + 16) / 16) * 16
+        R = [0] * int((d + (BLKSZ - 1)) / BLKSZ) * BLKSZ
 
         if T == None:
             T = self.ffx.twk
@@ -31,9 +32,7 @@ class Context:
              len(T) > self.ffx.maxtwklen)):
             raise RuntimeError('Input or tweak length error')
 
-        q = int((len(T) + b + 1 + 15) / 16) * 16
-
-        if enc:
+        if ENC:
             A = X[:u]
             B = X[u:]
         else:
@@ -47,13 +46,11 @@ class Context:
              10, u & 0xff,
              0, 0, 0, 0,
              0, 0, 0, 0]
-        P[8:12] = n.to_bytes(4, byteorder='big')
-        P[12:16] = len(T).to_bytes(4, byteorder='big')
+        P[-8:-4] = n.to_bytes(4, byteorder='big')
+        P[-4:] = len(T).to_bytes(4, byteorder='big')
 
-        Q = [0] * q
+        Q = [0] * (int((len(T) + b + 1 + 15) / BLKSZ) * BLKSZ)
         Q[:len(T)] = T
-
-        R = [0] * r
 
         nA = int(A, self.ffx.radix)
         nB = int(B, self.ffx.radix)
@@ -64,39 +61,41 @@ class Context:
             mV *= self.ffx.radix
 
         for i in range(10):
-            if int(enc) == i % 2:
-                mX = mV
-            else:
-                mX = mU
-
-            if enc:
+            if ENC:
                 Q[-b - 1] = i
             else:
                 Q[-b - 1] = 9 - i
 
             Q[-b:] = nB.to_bytes(b, byteorder='big')
 
-            R[:ffx.AES_BLOCK_SIZE] = list(self.ffx.prf(bytes(P + Q)))
+            R[:BLKSZ] = list(self.ffx.prf(bytes(P + Q)))
 
-            for j in range(int(len(R) / 16) - 1):
-                w = int.from_bytes(R[12:16], byteorder='big')
+            for j in range(int(len(R) / BLKSZ) - 1):
+                w = int.from_bytes(R[12:BLKSZ], byteorder='big')
+
                 w ^= j + 1
-                R[12:16] = w.to_bytes(4, byteorder='big')
-                R[16 * (j + 1):16 * (j + 2)] = list(
-                    self.ffx.ciph(bytes(R[:16])))
+                R[12:BLKSZ] = w.to_bytes(4, byteorder='big')
+
+                R[BLKSZ * (j + 1):BLKSZ * (j + 2)] = list(
+                    self.ffx.ciph(bytes(R[:BLKSZ])))
+
                 w ^= j + 1
-                R[12:16] = w.to_bytes(4, byteorder='big')
+                R[12:BLKSZ] = w.to_bytes(4, byteorder='big')
 
             y = int.from_bytes(R[:d], byteorder='big')
-            if enc:
+            if ENC:
                 y = nA + y
             else:
                 y = nA - y
 
             nA, nB = nB, nA
-            nB = y % mX
 
-        if enc:
+            if int(ENC) == i % 2:
+                nB = y % mV
+            else:
+                nB = y % mU
+
+        if ENC:
             Y = (ffx.NumberToString(nA, self.ffx.radix, u) +
                  ffx.NumberToString(nB, self.ffx.radix, v))
         else:
